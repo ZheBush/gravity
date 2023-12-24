924205424c6406a0aa22fc66f4e8aa2b49f0cdfe
Bushkov Evgeniy <buschkow2005@gmail.com>
2023-12-25 00:56:56 +0300

    git commit -m message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import {Flex, Circle, Button} from '@chakra-ui/react'
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+
+  const [positionX, setPositionX] = useState(0)
+  const [positionY, setPositionY] = useState(300)
+  const [flagForMove, setFlagForMove] = useState(false)
+  const [fields, setFields] = useState([])
+
+  const changePosition = () => {
+    const move = () => {
+      setPositionX((prevState) => prevState + 1)
+      setPositionY((prevState) => prevState + 0)
+    }
+    setTimeout(move, 100)
+  }
+
+  const changeItem = (event) => {
+    if (event.buttons == 4) {
+      setFlagForMove(!flagForMove) 
+    }
+    else if (event.buttons == 1) {
+      createFild(event.clientX, event.clientY)
+    }
+    else if (event.buttons == 2) {
+      deleteFild()
+    }
+  }
+
+  const createFild = () => {
+    const newField = {
+      
+    }
+  }
+
+  if (flagForMove) {
+    changePosition()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex
+      h = '100vh'
+      w = '100vw'
+      backgroundColor = 'rgb(50, 50, 50)'
+      position = 'absolute'
+      top = '0px'
+      left = '0px'
+      onMouseDown= {(event) => changeItem(event)}
+    >
+      <Circle
+        position = 'relative'
+        top = {positionY + 'px'}
+        left = {positionX + 'px'}
+        h = '30px'
+        w = '50px'
+        backgroundColor = 'rgb(0, 220, 255)'
+      >
+      </Circle>
+    </Flex>
   );
 }
-
-export default App;
