23199584e9fadda0de9c7c96c59851c26deaeb6d
Bushkov Evgeniy <buschkow2005@gmail.com>
2023-12-31 02:06:29 +0400

    git commit -m message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {Flex, Heading} from '@chakra-ui/react'
-import './styles.css'
+import {Flex, Heading, Grid} from '@chakra-ui/react'
+import CustomBox from './components/Boxes'
 
 const App = () => {
   return (
@@ -9,29 +9,46 @@ const App = () => {
       left = '0px'
       h = '100vh'
       w = '100vw'
-      bg = 'rgb(240 240 240)'
-    >
+      bg = 'rgb(40 40 40)'
+    > 
       <Heading
         position = 'absolute'
         left = '50vw'
+        color = 'rgb(240 240 240)'
       >
         Head
       </Heading>
       <Flex
         pos = 'absolute'
-        top = '10vh'
+        top = '17vh'
         left = '5vw'
       >
-        <Box className = 'tiles'
-          borderStyle = 'solid'
-          borderRadius = '30px'
-          borderColor = 'rgb(255 160 0)'
-          _hover = {{
-            bg: 'rgb(255 160 0)',
-            transition: '0.2s',
-            cursor: 'pointer'}}
+        <Grid
+          position = 'relative'
+          left = '7vw'
+          gap = '30px'
+          gridTemplateColumns = 'repeat(3, 1fr)'
         >
-      </Box>
+          <CustomBox
+            borderColor = 'rgb(250 160 0)'
+            hoverColor = 'rgb(250 160 0)' 
+            text = 'Полезные ссылки' />   
+          <CustomBox
+            borderColor = 'rgb(239 11 27)'
+            hoverColor = 'rgb(239 11 27)' />
+          <CustomBox
+            borderColor = 'rgb(0 215 250)'
+            hoverColor = 'rgb(0 215 250)' />    
+          <CustomBox
+            borderColor = 'rgb(2 201 42)'
+            hoverColor = 'rgb(2 201 42)' />
+          <CustomBox
+            borderColor = 'rgb(31 31 121)'
+            hoverColor = 'rgb(31 31 121)' />    
+          <CustomBox
+            borderColor = 'rgb(130 2 77)'
+            hoverColor = 'rgb(130 2 77)' />
+        </Grid>
       </Flex>
     </Flex>
   )
--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -3,11 +3,21 @@ import {Box} from '@chakra-ui/react'
 const CustomBox = (props) => {
     return (
         <Box
-         pos = 'absolute'
-         h = '28vh'
-         w = '20vw'
-         bg = {props.bgColor}
+            pos = 'relative'
+            h = '30vh'
+            w = '25vw'
+            bg = 'rgb(40 40 40)'
+            borderStyle = 'solid'
+            borderRadius = '30px'
+            borderColor = {props.borderColor}
+            color = 'rgb(255 255 255)'
+            _hover = {{
+                h: '35vh',
+                bg: props.hoverColor,
+                transition: '0.5s'
+            }}
         >
+            {props.text}
         </Box>
     )
 }
